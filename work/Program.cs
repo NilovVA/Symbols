028a6b60cbcc3517c0ac21c14912c773a2af3ca0
Nilovva <slavka11118@yandex.ru>
2024-01-17 21:22:49 +0400

    Добавьте файлы проекта.

---
+++ work/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class QueryAString
+{
+    public static void Main()
+    {
+        string aString = "ABCDE99F-J74-12-89A";
+
+        string number = Console.ReadLine();
+        int index = Convert.ToInt32(number);
+
+        
+        IEnumerable<char> stringQuery2 = aString.Skip(index - 1).Take(aString.Count());
+
+       
+        foreach (char c in stringQuery2)
+            Console.Write(c);
+
+        Console.WriteLine(System.Environment.NewLine + "Press any key to exit");
+        Console.ReadLine();
+    }
+}
+
+
+
+
+
+
+
+
